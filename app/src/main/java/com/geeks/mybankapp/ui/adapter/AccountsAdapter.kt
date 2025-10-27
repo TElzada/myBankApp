186a2d1183b643b5ec22fd37364a9744bbdd8e77
TElzada <elzada.turdubekova@alatoo.edu.kg>
2025-10-27 18:27:34 +0600

    Add click listener to open AccountDetailsActivity

--- app/src/main/java/com/geeks/mybankapp/ui/adapter/AccountsAdapter.kt
+++ app/src/main/java/com/geeks/mybankapp/ui/adapter/AccountsAdapter.kt
@@ -7,56 +7,53 @@ import com.geeks.mybankapp.data.model.Account
 import com.geeks.mybankapp.databinding.ItemAccountBinding
 
 class AccountsAdapter(
-    val onEdit:(Account) -> Unit,
-    val onSwitchToggle :(String, Boolean)->Unit,
-    val onDelete : (String)-> Unit
-): RecyclerView.Adapter<AccountsAdapter.AccountViewHolder>() {
+    val onEdit: (Account) -> Unit,
+    val onSwitchToggle: (String, Boolean) -> Unit,
+    val onDelete: (String) -> Unit,
+    val onItemClick: (String) -> Unit
+) : RecyclerView.Adapter<AccountsAdapter.AccountViewHolder>() {
+
     private val items = arrayListOf<Account>()
-    fun submitList(data: List<Account>){
+
+    fun submitList(data: List<Account>) {
         items.clear()
         items.addAll(data)
         notifyDataSetChanged()
-
     }
-    override fun onCreateViewHolder(
-        parent: ViewGroup,
-        viewType: Int
-    ): AccountsAdapter.AccountViewHolder {
-        val binding = ItemAccountBinding.inflate(LayoutInflater.from(
-            parent.context),parent,false)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AccountViewHolder {
+        val binding = ItemAccountBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false
+        )
         return AccountViewHolder(binding)
     }
 
-    override fun onBindViewHolder(holder: AccountsAdapter.AccountViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: AccountViewHolder, position: Int) {
         holder.bind(items[position])
     }
 
-    override fun getItemCount(): Int {
-        return items.size
-    }
-    inner class AccountViewHolder(private val binding: ItemAccountBinding): RecyclerView.ViewHolder(
-        binding.root){
-        fun bind(account: Account) = with(binding){
-            tvName.text = account.name
-            val text = "${account.balance} ${account.currency}"
-            tvBalance.text = text
-            btnEdit.setOnClickListener {
-                onEdit(account)
-            }
-            btnDelete.setOnClickListener {
-                account.id?.let{
-                    onDelete(it)
-                }
-            }
-            switcher.isChecked= account.isActive == true
-            switcher.setOnCheckedChangeListener { buttonView,isChecked->
-                account.id?.let{
-                    onSwitchToggle(it ,isChecked)
-                }
+    override fun getItemCount(): Int = items.size
 
+    inner class AccountViewHolder(private val binding: ItemAccountBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(account: Account) = with(binding) {
+            tvName.text = account.name
+            tvBalance.text = "${account.balance} ${account.currency}"
+
+            btnEdit.setOnClickListener { onEdit(account) }
+            btnDelete.setOnClickListener { account.id?.let { onDelete(it) } }
+
+            switcher.isChecked = account.isActive == true
+            switcher.setOnCheckedChangeListener { _, isChecked ->
+                account.id?.let { onSwitchToggle(it, isChecked) }
+            }
+
+            root.setOnClickListener {
+                account.id?.let { onItemClick(it) }
             }
         }
-
     }
-
 }
