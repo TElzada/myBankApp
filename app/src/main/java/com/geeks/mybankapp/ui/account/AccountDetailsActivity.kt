026068a299fd5413e1241fb1155cc83620a0d5ef
TElzada <elzada.turdubekova@alatoo.edu.kg>
2025-10-27 17:32:08 +0600

    "add detail screen"

---
+++ app/src/main/java/com/geeks/mybankapp/ui/account/AccountDetailsActivity.kt
@@ -0,0 +1,12 @@
+package com.geeks.mybankapp.ui.account
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.geeks.mybankapp.R
+
+class AccountDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_account_details)
+    }
+}
