84a249380e7d571b102e3ece4ef159207fcb9ad1
TElzada <elzada.turdubekova@alatoo.edu.kg>
2025-10-27 18:00:01 +0600

    rebase

--- app/src/main/java/com/geeks/mybankapp/di/NetworkModule.kt
+++ app/src/main/java/com/geeks/mybankapp/di/NetworkModule.kt
@@ -9,6 +9,7 @@ import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+
 @Module
 @InstallIn(SingletonComponent::class)
 object NetworkModule{
